323ca801d52204fd9d2ef9dba31adb6c200dca96
aj1063 <60145398+aj1063@users.noreply.github.com>
2020-01-21 20:50:35 +0530

    Create Day 1.cpp

---
+++ 30 Days/Day 1.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <iomanip>
+#include <limits>
+
+using namespace std;
+
+int main() {
+    int i = 4;
+    double d = 4.0;
+    string s = "HackerRank ";
+
+    int n;
+    double ds;
+    string sn;
+    cin>>n;
+    cin>>ds;
+    getline(cin,sn);
+    getline(cin,sn);
+    
+
+    cout<<n+i<<endl;
+    cout<<fixed<<setprecision(1)<<ds+d<<endl;
+    cout<<s<<sn<<endl;
+
+    // Declare second integer, double, and String variables.
+    
+    // Read and save an integer, double, and String to your variables.
+    
+    // Print the sum of both integer variables on a new line.
+    
+    // Print the sum of the double variables on a new line.
+    
+    // Concatenate and print the String variables on a new line
+    // The 's' variable above should be printed first.
+
+    return 0;
